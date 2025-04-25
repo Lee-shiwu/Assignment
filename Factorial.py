1a68e83c031a6e02f4dd6b7a85471f97d68bfcc0
shiwu li <leeshiwu66@gmail.com>
2025-04-26 09:47:12 +1200

    update

--- Factorial.py
+++ Factorial.py
@@ -1,8 +1,13 @@
 def factorial(n):
-    if n==0 or n==1:
+    if n<0:
+        print("Factorial is only defined for non-negative integers.")
+    elif n==0 or n==1:
         return 1
     else:
         return n*factorial(n - 1)
 x=int(input("Please enter a number to calculate factorial: "))
 result=factorial(x)
-print(result)
+if result is None:
+    pass
+else:
+    print(f"Factorial is {result}")
