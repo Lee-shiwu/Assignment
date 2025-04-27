d9f3b447165806b2cd2ce8ca0721ff3d1d325441
shiwu li <leeshiwu66@gmail.com>
2025-04-27 12:02:34 +1200

    redevelop

--- Week2/Factorial.py
+++ Week2/Factorial.py
@@ -1,12 +1,13 @@
-def factorial(n):
-    if n<0:
-        print("Factorial is only defined for non-negative integers.")
-    elif n==0 or n==1:
-        return 1
-    else:
-        return n*factorial(n - 1)
+class math_calculate:
+    def factorial(n):
+        if n<0:
+            print("Factorial is only defined for non-negative integers.")
+        elif n==0 or n==1:
+            return 1
+        else:
+            return n*math_calculate.factorial(n - 1)
 x=int(input("Please enter a number to calculate factorial: "))
-result=factorial(x)
+result=math_calculate.factorial(x)
 if result is None:
     pass
 else:
