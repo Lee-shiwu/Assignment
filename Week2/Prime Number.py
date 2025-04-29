f9e79a83fd5331828f25af2f4149c2b9e37665d4
shiwu li <leeshiwu66@gmail.com>
2025-04-29 21:10:10 +1200

    expand project

--- Week2/Prime Number.py
+++ Week2/Prime Number.py
@@ -1,4 +1,4 @@
-class Prime:
+class math_method:
     def prime_number(self,n):
         is_prime=True
         for i in range(2,int((n**0.5)+1)):
@@ -9,8 +9,17 @@ class Prime:
             print(f"{n} is a prime number")
         else:
             print(f"{n} is not a prime number")
+    def factorial (self,x):
+        if x<0:
+            print("Factorial is only defined for non-negative integers.")
+        elif x==0 or x==1:
+            return 1
+        else:
+            return x*self.factorial(x - 1)
         
 
-num= int(input("Please enter a number to check if it is prime number: "))
-p=Prime()
+num= int(input("Please enter a number to check if it is prime number or calculate factorial: "))
+p=math_method()
 p.prime_number(num)
+p.factorial(num)
+print(f"The factorial of {num} is {p.factorial(num)}")
